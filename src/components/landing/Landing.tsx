dc46fd221088a1c3dd1bc828fb0114a6c326bcd7
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2026-04-25 18:17:23 +0530

    feat(landing): compose all sections + emit landing_viewed

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,13 +1,56 @@
 "use client";
 
+import { useEffect } from "react";
+import { Header } from "./sections/Header";
+import { Hero } from "./sections/Hero";
+import { SocialProofStrip } from "./sections/SocialProofStrip";
+import { ProblemFraming } from "./sections/ProblemFraming";
+import { DemoShowcase } from "./sections/DemoShowcase";
+import { HowItWorks } from "./sections/HowItWorks";
+import { FeaturesByStage } from "./sections/FeaturesByStage";
+import { SampleReportPreview } from "./sections/SampleReportPreview";
+import { ComparisonTable } from "./sections/ComparisonTable";
+import { Testimonials } from "./sections/Testimonials";
+import { PricingTeaser } from "./sections/PricingTeaser";
+import { FAQ } from "./sections/FAQ";
+import { FinalCTA } from "./sections/FinalCTA";
+import { Footer } from "./sections/Footer";
+import { captureEvent } from "@/lib/analytics/posthog";
+
 export interface LandingProps {
   isLoggedIn: boolean;
 }
 
 export function Landing({ isLoggedIn }: LandingProps) {
-  // isLoggedIn will be used by section components in Task 28
-  void isLoggedIn;
-  return <main className="min-h-screen">{/* sections wired in Task 28 */}</main>;
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    captureEvent("landing_viewed", {
+      referrer: document.referrer || undefined,
+      utm_source: params.get("utm_source") || undefined,
+      utm_medium: params.get("utm_medium") || undefined,
+      utm_campaign: params.get("utm_campaign") || undefined,
+      utm_content: params.get("utm_content") || undefined,
+    });
+  }, []);
+
+  return (
+    <main className="min-h-screen">
+      <Header isLoggedIn={isLoggedIn} />
+      <Hero />
+      <SocialProofStrip />
+      <ProblemFraming />
+      <DemoShowcase />
+      <HowItWorks />
+      <FeaturesByStage />
+      <SampleReportPreview />
+      <ComparisonTable />
+      <Testimonials />
+      <PricingTeaser />
+      <FAQ />
+      <FinalCTA />
+      <Footer />
+    </main>
+  );
 }
 
 export default Landing;
